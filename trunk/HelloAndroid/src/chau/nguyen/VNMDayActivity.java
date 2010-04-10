706b025043573256a4904447cc5ea8e4d1f3a68f
nphaichau <nphaichau@a0850622-f4eb-11de-b359-dd99991af1b2>
2010-04-10 04:35:16 +0000

    completed select date feature

--- trunk/HelloAndroid/src/chau/nguyen/VNMDayActivity.java
+++ trunk/HelloAndroid/src/chau/nguyen/VNMDayActivity.java
@@ -1,25 +1,32 @@
 package chau.nguyen;
 
+import java.util.Calendar;
 import java.util.Date;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
+import android.widget.DatePicker;
 import android.widget.ViewSwitcher;
 import android.widget.Gallery.LayoutParams;
 import chau.nguyen.calendar.ui.VNMDayViewer;
 
 public class VNMDayActivity extends VNMCalendarViewActivity {
 	private static int MENU_MONTH_VIEW = 1;
-	private static int MENU_SETTINGS = 2;	
+	private static int MENU_SETTINGS = 2;
+	public static final int DATE_DIALOG_ID = 0;
+	private Date date;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vnm_day_activity);
+        this.date = new Date();
         this.switcher = (ViewSwitcher)findViewById(R.id.switcher);
         this.switcher.setFactory(this);
     }
@@ -62,6 +69,7 @@ public class VNMDayActivity extends VNMCalendarViewActivity {
 		
 		VNMDayViewer next = (VNMDayViewer)this.switcher.getNextView();
 		next.setDate(date);
+		this.date = date;
 		this.switcher.showNext();
 	}
 
@@ -76,4 +84,33 @@ public class VNMDayActivity extends VNMCalendarViewActivity {
 	@Override
 	public void onAnimationStart(Animation animation) {
 	}
+	
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(this.date);
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		int month = cal.get(Calendar.MONTH);
+		int year = cal.get(Calendar.YEAR);
+	    switch (id) {
+	    case DATE_DIALOG_ID:
+	        return new DatePickerDialog(this,
+	                    mDateSetListener,
+	                    year, month, day);
+	    }
+	    return null;
+	}
+	
+	// the callback received when the user "sets" the date in the dialog
+	private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			Calendar cal = Calendar.getInstance();
+			cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+			cal.set(Calendar.MONTH, monthOfYear);
+			cal.set(Calendar.YEAR, year);
+			VNMDayViewer currentView = (VNMDayViewer)switcher.getCurrentView();
+			currentView.setDate(cal.getTime());
+		}
+	};
 }
