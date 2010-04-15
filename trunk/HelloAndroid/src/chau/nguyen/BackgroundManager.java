045d01d80dee461597f5a8370ec31de63df7afcf
nphaichau <nphaichau@a0850622-f4eb-11de-b359-dd99991af1b2>
2010-04-15 09:02:00 +0000

    set background random

---
+++ trunk/HelloAndroid/src/chau/nguyen/BackgroundManager.java
@@ -0,0 +1,20 @@
+package chau.nguyen;
+
+import java.util.Calendar;
+
+public class BackgroundManager {
+	private static int[] backroundIds = new int[] {
+													R.drawable.body, 
+													R.drawable.body1, 
+													R.drawable.body2, 
+													R.drawable.body3, 
+													R.drawable.body4,
+													R.drawable.body5,
+													R.drawable.body6
+													
+													};
+	public static int getRandomBackgroundId() {
+		int index = Calendar.getInstance().get(Calendar.MILLISECOND) % backroundIds.length;
+		return backroundIds[index];
+	}
+}
