04c27809c918073be1d941f48524f858a63e4bb1
bpmtri@gmail.com <bpmtri@gmail.com@a0850622-f4eb-11de-b359-dd99991af1b2>
2010-04-16 14:59:12 +0000

    small improve about layout optimize backgournds

--- HelloAndroid/src/chau/nguyen/VNMDayActivity.java
+++ HelloAndroid/src/chau/nguyen/VNMDayActivity.java
@@ -66,7 +66,7 @@ public class VNMDayActivity extends VNMCalendarViewActivity {
     	menu.add(0, MENU_SELECT_TODAY, 0, "Hôm nay").setIcon(android.R.drawable.ic_menu_today);    	
     	menu.add(0, MENU_MONTH_VIEW, 0, "Xem tháng").setIcon(android.R.drawable.ic_menu_month);
     	menu.add(0, MENU_ADD_EVENT, 0, "Thêm sự kiện").setIcon(android.R.drawable.ic_menu_add);
-    	menu.add(0, MENU_ABOUT, 0, "Giới thiệu").setIcon(android.R.drawable.ic_menu_info_details);
+    	menu.add(0, MENU_ABOUT, 0, "Giới thiệu").setIcon(android.R.drawable.ic_menu_info_details);;
     	//menu.add(0, MENU_SETTINGS, 0, "Tùy chọn").setIcon(android.R.drawable.ic_menu_preferences);
     	return true;
     }
@@ -201,7 +201,7 @@ public class VNMDayActivity extends VNMCalendarViewActivity {
 	    		builder = new AlertDialog.Builder(this);
 	    		builder.setView(layout);
 	    		aboutDialog = builder.create();
-	    		aboutDialog.setTitle("Về 'Lịch Việt'");
+	    		aboutDialog.setTitle("Lịch Việt");
 	    		aboutDialog.setIcon(R.drawable.icon);
 	    		return aboutDialog;
 	    }
