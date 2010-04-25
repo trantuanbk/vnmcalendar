55a335af24ec271fa58c2f5a7ec4fc2a09cea4a0
bpmtri@gmail.com <bpmtri@gmail.com@a0850622-f4eb-11de-b359-dd99991af1b2>
2010-04-25 15:00:32 +0000

    add new small day widget

---
+++ trunk/HelloAndroid/src/chau/nguyen/calendar/appwidget/MonthWidgetConfigure.java
@@ -0,0 +1,12 @@
+package chau.nguyen.calendar.appwidget;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MonthWidgetConfigure extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
