eee7216ee8b2c6782afd352aa07610bb5dd684fa
nphaichau <nphaichau@a0850622-f4eb-11de-b359-dd99991af1b2>
2010-04-28 15:45:59 +0000

    implementing add event (follow lunar calendar)

--- trunk/HelloAndroid/src/chau/nguyen/VNMEventDetailsActivity.java
+++ trunk/HelloAndroid/src/chau/nguyen/VNMEventDetailsActivity.java
@@ -2,59 +2,308 @@ package chau.nguyen;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import android.app.Activity;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.AdapterView.OnItemSelectedListener;
 import chau.nguyen.calendar.VietCalendar;
 import chau.nguyen.calendar.ui.VNMDatePickerDialog;
 import chau.nguyen.calendar.ui.VNMDatePickerDialog.OnDateSetListener;
 
 public class VNMEventDetailsActivity extends Activity {
+	private static final int NO_REPEAT = 0;
+	private static final int DAILY_REPEAT = 1;
+	private static final int MONTHLY_REPEAT = 2;
+	private static final int YEARLY_REPEAT = 3;
+	private static final int REMIND_10_MINUTES = 0; // ten minutes
+	private static final int REMIND_1_HOUR = 1; // one hour = 60 minutes
+	private static final int REMIND_1_DAY = 2; // one day = 1440 minutes
+	private static Map<Integer, String> reminds;
+	private static Map<Integer, String> repeations;
 	private Date startDate;
 	private Date endDate;
-	private int startTime;
-	private int endTime;
 	protected EditText titleEditText;
+	protected EditText locationEditText;
+	protected EditText descriptionEditText;
 	protected Button startDateButton;
 	protected Button startTimeButton;
 	protected Button endDateButton;
 	protected Button endTimeButton;
+	protected Button saveButton;
+	protected Button discardButton;
+	protected Spinner repeatsDropDown;
+	protected Spinner remindersDropDown;
+	
+	static {
+		reminds = new HashMap<Integer, String>();
+		reminds.put(REMIND_10_MINUTES, "Nhắc trước 10 phút");
+		reminds.put(REMIND_1_HOUR, "Nhắc trước 1 giờ");
+		reminds.put(REMIND_1_DAY, "Nhắc trước 1 ngày");
+		
+		repeations = new HashMap<Integer, String>();
+		repeations.put(NO_REPEAT, "Chỉ một lần");
+		repeations.put(DAILY_REPEAT, "Hàng ngày");
+		repeations.put(MONTHLY_REPEAT, "Hàng tháng");
+		repeations.put(YEARLY_REPEAT, "Hàng năm");
+	}
+	
+	private static long getRemindTime(int id) {
+		switch (id) {
+			case REMIND_1_DAY:
+				return 1440;
+			case REMIND_1_HOUR:
+				return 60;
+			default:
+				return 10;
+		}
+	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.vnm_event_detail);
+		init();
+	}
+	
+	private void init() {
 		this.titleEditText = (EditText)findViewById(R.id.titleEditText);
+		this.descriptionEditText = (EditText)findViewById(R.id.descriptionEditText);
+		this.locationEditText = (EditText)findViewById(R.id.locationEditText);
 		this.startDateButton = (Button)findViewById(R.id.startDateButton);
 		this.startTimeButton = (Button)findViewById(R.id.startTimeButton);
 		this.endDateButton = (Button)findViewById(R.id.endDateButton);
 		this.endTimeButton = (Button)findViewById(R.id.endTimeButton);
+		this.saveButton = (Button)findViewById(R.id.saveButton);
+		this.discardButton = (Button)findViewById(R.id.discardButton);
+		this.remindersDropDown = (Spinner)findViewById(R.id.remindersDropDown);
+		this.repeatsDropDown = (Spinner)findViewById(R.id.repeatsDropDown);
 		Calendar cal = Calendar.getInstance();
 		this.startDate = cal.getTime();
-		this.startTime = cal.get(Calendar.HOUR_OF_DAY);
-		this.startTimeButton.setText(this.startTime + " giờ");
 		setDate(this.startDateButton, cal.getTime());
-		cal.add(Calendar.DAY_OF_MONTH, 1);
+		setTime(this.startTimeButton, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE));
+		cal.add(Calendar.HOUR_OF_DAY, 1);
 		this.endDate = cal.getTime();
-		this.endTime = cal.get(Calendar.HOUR_OF_DAY);
-		this.endTimeButton.setText(this.endTime + " giờ");
 		setDate(this.endDateButton, cal.getTime());
+		setTime(this.endTimeButton, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE));
 		this.startDateButton.setOnClickListener(new DateClickListener(this.startDateButton));
 		this.endDateButton.setOnClickListener(new DateClickListener(this.endDateButton));
+		this.startTimeButton.setOnClickListener(new TimeClickListener(this.startTimeButton));
+		this.endTimeButton.setOnClickListener(new TimeClickListener(this.endTimeButton));
 		
+		ArrayAdapter<String> repeatAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+		for (Integer item : repeations.keySet()) {
+			repeatAdapter.insert(repeations.get(item), item);
+		}
+		repeatAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		this.repeatsDropDown.setAdapter(repeatAdapter);
+		this.repeatsDropDown.setSelection(NO_REPEAT);
+		this.repeatsDropDown.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View view,
+					int position, long arg3) {
+				repeatsDropDown.setSelection(position);
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		ArrayAdapter<String> remindAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+		for (Integer item : reminds.keySet()) {
+			remindAdapter.insert(reminds.get(item), item);
+		}
+		remindAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		this.remindersDropDown.setAdapter(remindAdapter);
+		this.remindersDropDown.setSelection(REMIND_10_MINUTES);
+		this.remindersDropDown.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View view,
+					int position, long arg3) {
+				remindersDropDown.setSelection(position);
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		this.saveButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				String[] projection = new String[] { "_id", "name" };
+				Uri calendars = Uri.parse("content://calendar/calendars");
+				Cursor managedCursor = managedQuery(calendars, projection, "selected=1", null, null);
+				if (managedCursor.moveToFirst()) {
+					 String calId; 
+					 int idColumn = managedCursor.getColumnIndex("_id");
+					 do {
+					    calId = managedCursor.getString(idColumn);
+					    addEvent(calId);
+					 } while (managedCursor.moveToNext());
+				}
+				finish();
+			}
+			
+		});
+		
+		this.discardButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+			
+		});
+	}
+	
+	private void addEvent(String calId) {
+		ContentValues event = new ContentValues();
+		event.put("calendar_id", calId);
+		event.put("title", this.titleEditText.getText().toString());
+		event.put("description", this.descriptionEditText.getText().toString());
+		event.put("eventLocation", this.locationEditText.getText().toString());
+		Calendar calStart = Calendar.getInstance();
+		int currentYear = calStart.get(Calendar.YEAR);
+		calStart.setTime(this.startDate);
+		int startYear = calStart.get(Calendar.YEAR);
+		if (currentYear > startYear) {
+			calStart.set(Calendar.YEAR, currentYear);
+		}
+		Calendar calEnd = Calendar.getInstance();
+		calEnd.setTime(this.endDate);
+		int endYear = calEnd.get(Calendar.YEAR);
+		if (currentYear > endYear) {
+			calEnd.set(Calendar.YEAR, currentYear);
+		}
+		switch (this.repeatsDropDown.getSelectedItemPosition()) {
+		case YEARLY_REPEAT:
+			for (int i = 0; i <= 10; i++) {
+				calStart.add(Calendar.YEAR, i);
+				calEnd.add(Calendar.YEAR, i);
+				createEvent(event, calStart.getTime(), calEnd.getTime());
+			}
+			break;
+		
+		case MONTHLY_REPEAT:
+			for (int i = 0; i <= 120; i++) {
+				calStart.add(Calendar.MONTH, i);
+				calEnd.add(Calendar.MONTH, i);
+				createEvent(event, calStart.getTime(), calEnd.getTime());
+			}
+		break;
+			
+		case DAILY_REPEAT:
+			for (int i = 0; i <= 3600; i++) {
+				calStart.add(Calendar.DAY_OF_MONTH, i);
+				calEnd.add(Calendar.DAY_OF_MONTH, i);
+				createEvent(event, calStart.getTime(), calEnd.getTime());
+			}
+			break;
+		default:
+			createEvent(event, this.startDate, this.endDate);
+			break;
+		}
+		
+
+	}
+	
+	private void createEvent(ContentValues event, Date startDate, Date endDate) {
+		ContentResolver cr = getContentResolver();
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(startDate);
+		int lunarDay = cal.get(Calendar.DAY_OF_MONTH);
+		int lunarMonth = cal.get(Calendar.MONTH);
+		int lunarYear = cal.get(Calendar.YEAR);
+		int temp[] = VietCalendar.convertLunar2Solar(lunarDay, lunarMonth, lunarYear);
+		cal.set(Calendar.DAY_OF_MONTH, temp[0]);
+		cal.set(Calendar.MONTH, temp[1]);
+		cal.set(Calendar.YEAR, temp[2]);
+		Date solarStartDate = cal.getTime();
+		
+		cal.setTime(endDate);
+		lunarDay = cal.get(Calendar.DAY_OF_MONTH);
+		lunarMonth = cal.get(Calendar.MONTH);
+		lunarYear = cal.get(Calendar.YEAR);
+		temp = VietCalendar.convertLunar2Solar(lunarDay, lunarMonth, lunarYear);
+		cal.set(Calendar.DAY_OF_MONTH, temp[0]);
+		cal.set(Calendar.MONTH, temp[1]);
+		cal.set(Calendar.YEAR, temp[2]);
+		Date solarEndDate = cal.getTime();
+		long startTime = solarStartDate.getTime();
+		long endTime = solarEndDate.getTime();
+		event.put("dtstart", startTime);
+		event.put("dtend", endTime);
+		
+		event.put("hasAlarm", 1);
+		try {
+			Uri eventsUri = Uri.parse("content://calendar/events");
+			Uri newEvent = cr.insert(eventsUri, event);
+			long id = Long.parseLong(newEvent.getLastPathSegment());
+			if(newEvent != null) {
+				long reminderMinutes = getRemindTime(remindersDropDown.getSelectedItemPosition());
+
+				ContentValues values = new ContentValues();
+				values.put("event_id", id);
+				values.put("method", 1);
+				values.put("minutes", reminderMinutes);
+				cr.insert(Uri.parse("content://calendar/reminders"), values);
+
+//				ContentValues alertValues = new ContentValues();
+//				alertValues.put("event_id", id);
+//				alertValues.put("begin", startTime);
+//				alertValues.put("end", endTime);
+//				alertValues.put( "alarmTime", startTime);
+//				alertValues.put( "state", 0 );
+//				alertValues.put( "minutes", reminderMinutes );
+//				cr.insert( Uri.parse( "content://calendar/calendar_alerts" ), alertValues );
+				} 
+		} catch (Exception e) {
+			Log.e("AddingCalendarEvent", e.getMessage());
+		}
 	}
 	
 	private void setDate(TextView view, Date date) {
 		view.setText(VietCalendar.formatVietnameseDate(date));
 	}
 	
-	private void setTime(TextView view, int time) {
-		view.setText(time + " giờ");
+	private void setTime(TextView view, int hourOfDay, int minute) {
+		String hour = hourOfDay + "";
+		String minuteInText = minute + "";
+		if (hourOfDay < 10)
+			hour = "0" + hour;
+		if (minute < 10)
+			minuteInText = "0" + minuteInText;
+			
+		view.setText(hour + ":" + minuteInText);
 	}
+	
 	private class DateClickListener implements View.OnClickListener {
 		 private View view;
 		 public DateClickListener(View view) {
@@ -90,11 +339,57 @@ public class VNMEventDetailsActivity extends Activity {
 			cal.set(Calendar.MONTH, monthOfYear);
 			cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
 			if (this.dialog.equals(START_DATE)) {
+				startDate = cal.getTime();
 				setDate(startDateButton, cal.getTime());
 			} else {
+				endDate = cal.getTime();
 				setDate(endDateButton, cal.getTime());
 			}
 		}
 		
 	}
+	
+	private class TimeClickListener implements View.OnClickListener {
+		 private View view;
+		 public TimeClickListener(View view) {
+			 this.view = view;
+		 }
+		 public void onClick(View v) {
+			 if (view == startTimeButton) {
+				 new TimePickerDialog(VNMEventDetailsActivity.this, new TimeSetListener(TimeSetListener.START_TIME), 7, 0, true).show();
+			 } else {
+				 new TimePickerDialog(VNMEventDetailsActivity.this, new TimeSetListener(TimeSetListener.END_TIME), 7, 0, true).show();
+			 }
+		 }
+	}
+	
+	private class TimeSetListener implements OnTimeSetListener {
+		private static final String START_TIME = "startTime";
+		private static final String END_TIME = "endTime";
+		private String dialog;
+		
+		public TimeSetListener(String dialog) {
+			this.dialog = dialog;
+		}
+
+		@Override
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			Calendar cal = Calendar.getInstance();
+			if (START_TIME.equals(this.dialog)) {
+				cal.setTime(startDate);
+				cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
+				cal.set(Calendar.MINUTE, minute);
+				startDate = cal.getTime();
+				setTime(startTimeButton, hourOfDay, minute);
+			} else {
+				cal.setTime(endDate);
+				cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
+				cal.set(Calendar.MINUTE, minute);
+				endDate = cal.getTime();
+				setTime(endTimeButton, hourOfDay, minute);
+			}
+		}
+		
+	}
+	
 }
