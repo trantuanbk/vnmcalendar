8b9786f439582d56d931ec39a447b84a5fdcaea2
bpmtri@gmail.com <bpmtri@gmail.com@a0850622-f4eb-11de-b359-dd99991af1b2>
2010-05-14 08:00:24 +0000

    version 1.4

--- trunk/HelloAndroid/src/chau/nguyen/calendar/appwidget/ThemeManager.java
+++ trunk/HelloAndroid/src/chau/nguyen/calendar/appwidget/ThemeManager.java
@@ -3,7 +3,6 @@ package chau.nguyen.calendar.appwidget;
 import java.util.HashMap;
 
 import android.content.Context;
-import android.util.Log;
 import chau.nguyen.R;
 import chau.nguyen.calendar.ui.MonthViewRenderer;
 
@@ -15,16 +14,20 @@ public class ThemeManager {
 		themeMap = new HashMap<String, Integer>();
 		themeMap.put("light", R.raw.light);
 		themeMap.put("dark", R.raw.dark);
+		themeMap.put("transparent_light", R.raw.transparent_light);
+		themeMap.put("transparent_dark", R.raw.transparent_dark);
 		themeMap.put("small_light", R.raw.small_light);
 		themeMap.put("small_dark", R.raw.small_dark);
+		themeMap.put("small_transparent_light", R.raw.small_transparent_light);
+		themeMap.put("small_transparent_dark", R.raw.small_transparent_dark);
 	}
 	
-	public static MonthViewRenderer.Config getConfig(Context context, String theme) {
+	public static synchronized MonthViewRenderer.Config getConfig(Context context, String theme) {
 		if (instance == null) {
 			instance = new ThemeManager();
 		}
-		theme = theme.toLowerCase();
-		Log.d("DEBUG", "Theme name: " + theme);
-		return MonthViewRenderer.Config.load(context.getResources().openRawResource(instance.themeMap.get(theme)));
+		theme = theme.toLowerCase();		
+		return MonthViewRenderer.Config.load(context.getResources().openRawResource(instance.themeMap.get(theme)),
+				context);
 	}
 }
