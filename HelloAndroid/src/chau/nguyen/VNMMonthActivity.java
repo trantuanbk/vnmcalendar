452609a6f249a5846bff01052b246cd1e96d4484
nphaichau <nphaichau@a0850622-f4eb-11de-b359-dd99991af1b2>
2011-04-25 03:28:28 +0000

    adding adMob view

--- HelloAndroid/src/chau/nguyen/VNMMonthActivity.java
+++ HelloAndroid/src/chau/nguyen/VNMMonthActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import chau.nguyen.calendar.ui.ScrollableMonthView;
 import chau.nguyen.calendar.ui.MonthView.OnDateSelectedListener;
 import chau.nguyen.calendar.ui.ScrollableMonthView.OnDateChangedListener;
@@ -26,6 +28,12 @@ public class VNMMonthActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);        
         BackgroundManager.init(this);
+        
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+        		WindowManager.LayoutParams.FLAG_FULLSCREEN);
         
         this.scrollView = new HorizontalScrollView(this);
         this.setContentView(this.scrollView);
